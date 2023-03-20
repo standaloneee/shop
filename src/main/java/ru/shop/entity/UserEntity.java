40cd81dd019d108a6b95ef4cfaf46786f7fa80a3
standaloneee <tzherzdev@gmail.com>
2023-03-20 12:21:35 +0300

    UserEntity init

---
+++ src/main/java/ru/shop/entity/UserEntity.java
@@ -0,0 +1,28 @@
+package ru.shop.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    private UUID id;
+
+    private String email;
+
+    private String password_hash;
+
+    private double balance;
+}
