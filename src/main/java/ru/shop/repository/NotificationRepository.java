355439cf5ec307ce58e634476f684a5a3e015590
standaloneee <tzherzdev@gmail.com>
2023-03-22 21:02:43 +0300

    removed unused imports

--- src/main/java/ru/shop/repository/NotificationRepository.java
+++ src/main/java/ru/shop/repository/NotificationRepository.java
@@ -3,9 +3,7 @@ package ru.shop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.shop.entity.Notification;
-import ru.shop.entity.Tag;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
--- src/main/java/ru/shop/service/NotificationService.java
+++ src/main/java/ru/shop/service/NotificationService.java
@@ -3,14 +3,7 @@ package ru.shop.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.shop.entity.Notification;
-import ru.shop.entity.Tag;
-import ru.shop.exception.TagNotFoundException;
 import ru.shop.repository.NotificationRepository;
-import ru.shop.repository.TagRepository;
-
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
